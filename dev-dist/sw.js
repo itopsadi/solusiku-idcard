0e63c6c61528465b51c1a486d39631377a8ba56e
Hibah Gigih <hibahgigih@gmail.com>
2026-05-13 15:07:34 +0700

    ok

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.edet9q9t3ac"
+    "revision": "0.3ak2d7viks"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
